080b8b9ce905b3cf22012cdf94849773f1197e13
hyuk <dlsgur0906@gmail.com>
2022-02-07 16:10:59 +0900

    first commit

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+const content = `안녕하세요, 프론트엔드 개발자 이인혁입니다.`;
+const text = document.querySelector('.title');
+let index = 0;
+
+let handle = setInterval(function typing(){
+    text.textContent += content[index++];
+    if(index >= content.length){
+        clearInterval(handle);
+        return;
+    }
+}, 150);
